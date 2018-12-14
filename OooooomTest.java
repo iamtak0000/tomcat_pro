2370065c153332884a313714d60f5118ca4f86d6
iamtak0000 <45733332+iamtak0000@users.noreply.github.com>
2018-12-14 15:39:06 +0900

    Add files via upload

---
+++ OooooomTest.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+public class OooooomTest {
+
+   private static class ApiRequest {
+       private String core = "politics";
+   }
+
+   public static void main(String[] args){
+       List<ApiRequest> omg = new ArrayList<ApiRequest>();
+       while(true) {
+           omg.add(new ApiRequest());
+       }
+
+   }
+
+}
